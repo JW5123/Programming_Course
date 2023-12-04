ee9afcf0cd71242bfd9516a644fa86b2e191c70f
JW5123 <Jay2002714@gmail.com>
2023-12-05 00:17:18 +0800

    Update 5-32.c & 5-33.c

--- HW-2/D1273865_5-32.c
+++ HW-2/D1273865_5-32.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define MAX 100
+#define MAX 1000
 
 void PlayGame(){
 
@@ -41,3 +41,12 @@ int main(){
     return 0;
 }
 
+/*
+
+建立一個無回傳值的猜數字函式，設定隨機亂數1~1000，接著使用迴圈當猜測數字不等於結果時持續輸入，
+當輸入的數大於結果時印出too high，小於結果時顯示too low，等於結果時顯示Exxellent並退出回圈
+主程式內先設定亂數種子型別為無號整數，接著使用do while先執行程式，一開始設定system("cls")用以清除畫面，
+接著呼叫猜數字函式進行遊戲直到猜對數字後，並詢問是否繼續遊戲，這裡使用getchar()函式用以接收輸入的字元，
+最後判斷輸入數字是y還是n，若是y則繼續遊戲，n則退出結束程式
+
+*/
