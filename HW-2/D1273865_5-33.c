3ce8b6c231dcd81e6d10cf11f95b53a5a6759317
JW5123 <Jay2002714@gmail.com>
2023-12-04 23:35:19 +0800

    Update 5-33.c

--- HW-2/D1273865_5-33.c
+++ HW-2/D1273865_5-33.c
@@ -6,19 +6,34 @@
 void PlayGame(){
 
     int number = rand() % MAX + 1;
-    int guess = 0;
+    int guess = 0, t = 0;
     printf("Guess Number 1 ~ %d\n", MAX);
     while(guess != number){
         printf("=> ");
         scanf("%d", &guess);
-        if(guess > number)
+        if(guess > number){
             printf("Too high! Try again.\n");
-        else if(guess < number)
+            t++;
+        }
+        else if(guess < number){
             printf("Too low! Try again.\n");
+            t++;
+        }
         else{
             printf("Excellent! You guessed the number!\n");
+            t++;
             break;
         }
+        printf("times: %d\n", t);
+    }
+    if(t <= 10){
+        printf("Either you know the secret or yougot lucky!\n");
+    }
+    else if(t > 10){
+        printf("You should be able to do better!\n");
+    }
+    else{
+        printf("Ahah! You know the secret!\n");
     }
 }
 
